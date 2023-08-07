67f4c831fbdba93c348fe9aeb351362a4f8957e8
Mohd Danish <112017155+MOHDDANISH007@users.noreply.github.com>
2023-08-07 20:05:15 +0530

    Update Majority_Element.cpp

--- Majority_Element.cpp
+++ Majority_Element.cpp
@@ -1,30 +1,30 @@
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        int  count = 1, res =0;
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i] == nums[res]){
-                count ++;
-            }
-            else{
-                count --;
-            }
-            if(count == 0){
-                count = 1;
-                res = i;
-            }
-        }
-        count = 0;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[count] == nums[i]){
-                count ++;
-            }
-        }
-        if(count > (nums.size()/2) ){
-            return nums[res];
-        }
-        else{
-            return -1;
+int majorityElement(vector<int> v) {
+	//finding which element is in majority case but u don't know how many times
+    // come in array..
+    int count =1, res = 0;
+    for(int i=1; i<v.size(); i++){
+      if (v[i] == v[res]) {
+        count++;
+      }
+      else{
+        count --;
+      }
+      if(count == 0){
+          res = i;
+          count = 1;
+      }
+    }
+    // count how many time it comes
+    count = 0;
+    for(int i=0; i<v.size(); i++){
+        if(v[i] == v[res]){
+            count ++;
         }
     }
-};
+    if(count > v.size()/2){
+        return v[res];
+    }
+    else{
+        return  -1;
+    }
+}
