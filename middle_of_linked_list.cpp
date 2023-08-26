d0a0a318058a925b353794ea9b4b9bff542fe068
Mohd Danish <m.danish00707@gamail.com>
2023-08-27 00:10:51 +0530

    Added middle element of linked list program

---
+++ middle_of_linked_list.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getlength(ListNode* head){
+    int len = 0;
+    ListNode* temp = head;
+    while(temp != NULL){
+        len++;
+        temp = temp->next;
+    }
+    return len;
+}
+    ListNode* middleNode(ListNode* head) {
+    int len = getlength(head);
+    int ans = (len/2);
+    int cnt = 0;
+    ListNode* temp = head;
+    while(cnt < ans){
+        temp = temp->next;
+        cnt++;
+    }
+    return temp;
+    }
+};
