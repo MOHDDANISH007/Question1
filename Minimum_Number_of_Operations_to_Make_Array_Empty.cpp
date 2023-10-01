fb570111dd44a884f14e8f936b3ecf6cf4b04683
Mohd Danish <m.danish00707@gamail.com>
2023-10-01 23:40:15 +0530

    Added try to array empty in minimum numbers of operation in c++

---
+++ Minimum_Number_of_Operations_to_Make_Array_Empty.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map <int, int > mp;
+
+        for(auto it : nums){
+            mp[it]++;
+        }
+
+        int count = 0; 
+        for(auto it: mp){
+            if(it.second == 1){
+                return -1;
+            }
+            if(it.second % 3 == 0){
+                count += it.second / 3;
+            }
+            else{
+                count += it.second / 3 + 1;
+            }
+        }
+        return count;
+
+    }
+};
