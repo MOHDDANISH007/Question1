2f6e5a76c6f4ae58fceba2c1d8a12e537857de7f
Mohd Danish <m.danish00707@gamail.com>
2023-10-22 02:45:29 +0530

    Added XOR Calculating program in c++

---
+++ Calculating_XOR.cpp
@@ -0,0 +1,31 @@
+int calculateXOR(int n)
+
+{
+
+    // Write your code here.
+
+    if(n%4==0){
+
+        return n;
+
+    }
+
+    if(n%4==3){
+
+        return 0;
+
+    }
+
+    if(n%4==1){
+
+        return 1;
+
+    }
+
+    if(n%4==2){
+
+        return n+1;
+
+    }
+
+}
