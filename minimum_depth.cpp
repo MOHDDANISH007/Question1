22e5678970a15947a45452e0658241ea24c61d0f
Mohd Danish <m.danish00707@gamail.com>
2023-11-26 22:41:05 +0530

    Added minimum depth in c++

---
+++ minimum_depth.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root) {
+            return 0; // Empty tree has depth 0
+        }
+
+        int cnt = 1;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int levelSize = q.size(); // Number of nodes at the current level
+            for (int i = 0; i < levelSize; ++i) {
+                TreeNode* temp = q.front();
+                q.pop();
+
+                if (temp->left) {
