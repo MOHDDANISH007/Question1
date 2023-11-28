7ca18461f413213cc948aec2097135acc4c3a945
Mohd Danish <112017155+MOHDDANISH007@users.noreply.github.com>
2023-11-29 00:03:53 +0530

    Update same_tree.cpp

--- same_tree.cpp
+++ same_tree.cpp
@@ -56,3 +56,16 @@ public:
     }
 };
 
+// Opttimization approach
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr || q == nullptr){
+            return (p==q);
+        }
+        else{
+            return (p->val == q->val) && (isSameTree(p->left,q->left)) && (isSameTree(p->right, q->right)); 
+        }
+    }
+};
