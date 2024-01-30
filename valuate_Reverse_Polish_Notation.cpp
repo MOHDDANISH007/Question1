b6d46cbb6cb3adfd1e65fbeae87641819cda2340
Mohd Danish <m.danish00707@gamail.com>
2024-01-30 23:45:14 +0530

    Added valuate_Reverse_Polish_Notation

---
+++ valuate_Reverse_Polish_Notation.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+
+        for (const auto& token : tokens) {
+            if (token == "+") {
+                int b = st.top(); 
+                st.pop();
+
+                int a = st.top();
+                st.pop();
+                
+                st.push(a + b);
+            } else if (token == "-") {
+                int b = st.top(); 
+                st.pop();
+
+                int a = st.top();
+                 st.pop();
+
+                st.push(a - b);
+            } else if (token == "*") {
+                int b = st.top(); 
+                st.pop();
+
+                int a = st.top(); 
+                st.pop();
+
+                st.push(a * b);
+            } else if (token == "/") {
+                int b = st.top(); 
+                st.pop();
+
+                int a = st.top(); 
+                st.pop();
+                
+                st.push(a / b);  
+            } else {
+                st.push(stoi(token));
+            }
+        }
+
+        return st.top();
+    }
+};
