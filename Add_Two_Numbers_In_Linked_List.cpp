7cf962e85c082150f1f15e032239807b267e5268
Mohd Danish <m.danish00707@gamail.com>
2024-03-02 01:17:19 +0530

    Added Two number in linked List

---
+++ Add_Two_Numbers_In_Linked_List.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
+        // creating dummy node;
+
+        ListNode* dummy = new ListNode(-1);
+        ListNode* cur = dummy; 
+
+        while(temp1 != nullptr || temp2 != nullptr){
+            int sum = carry;
+            if(temp1 != nullptr){
+                sum += temp1->val;
+            }
+            if(temp2 != nullptr){
+                sum += temp2->val;
+            }
+
+            ListNode* node = new ListNode(sum % 10);
+            carry = sum/10;
+
+           cur->next = node;
+           cur = node;
+
+            if(temp1 != nullptr){
+                temp1 = temp1->next;
+            } 
+
+            if(temp2 != nullptr){
+                temp2 = temp2->next;
+            } 
+        }
+
+        if(carry != 0){
+            ListNode* node = new ListNode(carry);
+            cur->next = node;
+           cur = node;
+        }
+
+        return dummy->next;
+    }
+};
