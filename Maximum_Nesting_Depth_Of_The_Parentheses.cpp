4022d68481d6d080f72913893eec929d014dd0ae
Mohd Danish <m.danish00707@gamail.com>
2024-04-04 22:33:05 +0530

    Maximum Nesting Depth of the Parentheses

--- Maximum_Nesting_Depth_Of_The_Parentheses.cpp
+++ Maximum_Nesting_Depth_Of_The_Parentheses.cpp
@@ -1,16 +1,30 @@
-int maxDepth(string s) {
-	int ans = 0;
-	int maxi = INT_MIN;
-	int n = s.size();
-	
-	for(int i=0; i<n; i++){
-		if(s[i] == '('){
-			ans++;
-			maxi = max(ans,maxi);
-		}
-		else if(s[i] == ')'){
-			ans--;
-		}
-	}
-	return maxi;
-}
+#include <stack>
+#include <string>
+#include <algorithm>
+
+class Solution {
+public:
+    int maxDepth(string s) {
+        if (s.empty()) {
+            return 0;
+        }
+
+        stack<char> st;
+        int depth = 0;
+        int ans = 0;
+
+        for (char ch : s) {
+            st.push(ch);
+
+            if (ch == '(') {
+                depth++;
+            }
+            else if (ch == ')') {
+                depth--;
+            }
+            ans = max(ans, depth);
+        }
+        return ans;
+    }
+};
+
